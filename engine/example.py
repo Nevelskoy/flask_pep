ca17f2024e50754c5bba1a0fa1ece071da05e1d5
Sadovskiy <safelikc@gmail.com>
2023-01-25 08:15:20 +0300

    add routes get, post

--- engine/example.py
+++ engine/example.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Welcome to Flask from Hexlet!'
+
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users, Hello bro!'
